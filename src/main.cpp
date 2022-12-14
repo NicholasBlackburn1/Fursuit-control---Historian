5a12b66a23f5cce876ce817cb32516c6600c9fad
Nick Blackburn <nickblackburn02@gmail.com>
2022-12-14 00:24:50 -0500

    trying o get comms workign

--- src/main.cpp
+++ src/main.cpp
@@ -2,28 +2,29 @@
 #include <SoftwareSerial.h> 
 #include <ArduinoJson.h>
 
+SoftwareSerial MyBlue;
 
 // bluetooth module config setup
 void config_bluetooth(){
 
 SoftwareSerial MyBlue(2, 3); // RX | TX 
-MyBlue.begin(9600);
+Blue.begin(9600);
 Serial.begin(9600);
 
 Serial.println("Starting Historian....");
 
 serial.println("waiting for ble module");
 
-//
-if (MyBlue.available()){
+if (Blue.available()){
   Serial.println("Bluetooth Ready to connect\nDefualt password is 1234 or 000"); 
+  Serial.println("waiting for client to connect");
 }
-
 
 }
 
 void setup() {
 config_bluetooth();
+
 
 
 }
