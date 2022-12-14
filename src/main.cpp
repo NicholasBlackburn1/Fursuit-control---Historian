cbf5e8852323e8278d7a8d242a84b56e645b0625
Nick Blackburn <nickblackburn02@gmail.com>
2022-12-14 00:11:30 -0500

    setting up bluetooth serial

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h> 
+#include <ArduinoJson.h>
+
+
+//
+void config_bluetooth(){
+
+  
+  
+SoftwareSerial MyBlue(2, 3); // RX | TX 
+MyBlue.begin(9600); 
+Serial.println("Ready to connect\nDefualt password is 1234 or 000"); 
+
+}
 
 void setup() {
-  // put your setup code here, to run once:
+
+Serial.begin(9600);
+
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
